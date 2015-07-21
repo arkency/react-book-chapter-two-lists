d51ec26ecb2ae15f39747846422323ff09ec574d
Piotr Macuk <piotr@macuk.pl>
2015-07-21 14:35:12 +0200

    List as separated component

--- src/two-lists.js
+++ src/two-lists.js
@@ -1,9 +1,15 @@
+class List extends React.Component {
+  render() {
+    return (<span>{this.props.name}</span>);
+  }
+}
+
 class TwoLists extends React.Component {
   render() {
     return (
       <div id={this.props.id}>
-        <span>List 1</span>
-        <span>List 2</span>
+        <List name="Brand" />
+        <List name="Model" />
       </div>
     );
   }
